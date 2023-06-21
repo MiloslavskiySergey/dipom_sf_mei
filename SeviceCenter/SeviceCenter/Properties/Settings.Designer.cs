6660925710142d549bd0198fc1b549da49414022
MiloslavskiySergey <sergey07061999@gmail.com>
2023-06-22 00:52:45 +0300

    feat: presentation

--- SeviceCenter/SeviceCenter/Properties/Settings.Designer.cs
+++ SeviceCenter/SeviceCenter/Properties/Settings.Designer.cs
@@ -94,5 +94,17 @@ namespace SeviceCenter.Properties {
                 this["DbName"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool IsFirstLaunch {
+            get {
+                return ((bool)(this["IsFirstLaunch"]));
+            }
+            set {
+                this["IsFirstLaunch"] = value;
+            }
+        }
     }
 }
